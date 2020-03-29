c48660da79ff4cfcd4a5d086b8c617c386a06488
satyendra gupta <sat8ndra@gmail.com>
2020-03-30 01:38:15 +0530

    state change by onclick button

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: 'Hello Manoj'
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello my name is satyendra</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: 'Hello Raj' })}>
+            Change Text
+          </button>
         </header>
       </div>
     );
