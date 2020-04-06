c14a4aa049b4607356ebb3f937923820c50ff7be
satyendra gupta <sat8ndra@gmail.com>
2020-04-07 01:25:42 +0530

    project monsters rolodex complete

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
-    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,9 +18,9 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value });
-  }
+  };
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -29,6 +28,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
