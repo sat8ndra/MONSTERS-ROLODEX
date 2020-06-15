d7e2e464f8ddbb251b41f4ec95ac768959cd87e7
satyendra gupta <sat8ndra@gmail.com>
2020-06-16 01:14:22 +0530

    adding files for github pages

---
+++ build/precache-manifest.6b0fb0c4990d2b526fa5db1be4291134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "859757a498a2dc64a87013416862c823",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f26b823724bba88bd728",
+    "url": "/monsters-rolodex/static/css/main.aed00cb8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26b823724bba88bd728",
+    "url": "/monsters-rolodex/static/js/main.4dd73a4e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
